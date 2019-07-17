2445407f6edcfe61a27c04bf373d52013ba6281d
Sarah Ng <sarah.ng26@gmail.com>
2019-07-16 23:26:46 -0700

    Updates

---
+++ precache-manifest.89ac95ab9247af0a03b3f6f28be9ed0a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cac3c6c5fa3557b0a98b302979a6991",
+    "url": "/gh-issues-viewer/index.html"
+  },
+  {
+    "revision": "85684080169707b5bf71",
+    "url": "/gh-issues-viewer/static/css/main.7c04d26b.chunk.css"
+  },
+  {
+    "revision": "90d2b51aca1f68a5bc09",
+    "url": "/gh-issues-viewer/static/js/2.e8e72ac7.chunk.js"
+  },
+  {
+    "revision": "85684080169707b5bf71",
+    "url": "/gh-issues-viewer/static/js/main.c3dd6b62.chunk.js"
+  },
+  {
+    "revision": "a02653754bbe852ccae1",
+    "url": "/gh-issues-viewer/static/js/runtime~main.6ecd0e19.js"
+  },
+  {
+    "revision": "49c092514c29c82004792213332c11ee",
+    "url": "/gh-issues-viewer/static/media/close.49c09251.svg"
+  },
+  {
+    "revision": "29eed3f5d6b808b475e2a5a51b62d65a",
+    "url": "/gh-issues-viewer/static/media/issue-closed.29eed3f5.svg"
+  },
+  {
+    "revision": "45af372786e4fc6d6c54a88018cd6ce5",
+    "url": "/gh-issues-viewer/static/media/montserrat-bold.45af3727.woff2"
+  },
+  {
+    "revision": "b760b19f49a84fd4b3ce2252969dec30",
+    "url": "/gh-issues-viewer/static/media/montserrat-bold.b760b19f.woff"
+  },
+  {
+    "revision": "a820606bee2646d5ec81751628904dd4",
+    "url": "/gh-issues-viewer/static/media/montserrat-light.a820606b.woff"
+  },
+  {
+    "revision": "d0d8af75aedd27ba147c6b6ad7d2eba7",
+    "url": "/gh-issues-viewer/static/media/montserrat-light.d0d8af75.woff2"
+  },
+  {
+    "revision": "3fb4d2902124120fbdc713a267b36403",
+    "url": "/gh-issues-viewer/static/media/montserrat-regular.3fb4d290.woff"
+  },
+  {
+    "revision": "b94d4bb6c9c96d53b7a724f6d7397b49",
+    "url": "/gh-issues-viewer/static/media/montserrat-regular.b94d4bb6.woff2"
+  },
+  {
+    "revision": "f7396a8d8ececa8b625f9eba428288fe",
+    "url": "/gh-issues-viewer/static/media/pull-request.f7396a8d.svg"
+  },
+  {
+    "revision": "4d4782aeaf28efdf222a2b82c8b73283",
+    "url": "/gh-issues-viewer/static/media/search.4d4782ae.svg"
+  }
+]);
