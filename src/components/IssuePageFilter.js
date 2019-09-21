a2a0efdc92e28459cf5234dad06210f3d114b0b9
Sarah Ng <sarah.ng26@gmail.com>
2019-09-20 17:42:45 -0700

    Converted IssuePageFilter to hook, added initial filter in constants

--- src/components/IssuePageFilter.js
+++ src/components/IssuePageFilter.js
@@ -1,102 +1,53 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import closeIcon from '../_assets/icons/close.svg';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { closeIssueViewer, setFilter } from '../actions';
+import { FILTERS } from '../constants/constants';
 
-class IssuePageFilter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filterStyle: {
-        all: 'issue-page-filter-selected',
-        open: 'issue-page-filter-unselected',
-        closed: 'issue-page-filter-unselected',
-        pulls: 'issue-page-filter-unselected',
+function IssuePageFilter () {
+  const selectedStyle = 'issue-page-filter-selected';
+  const unselectedStyle = 'issue-page-filter-unselected';
+  const [filterStyle, setFilterStyle] = useState(FILTERS);
+
+  const dispatch = useDispatch();
+
+  const handleCloseViewer = () => {
+    dispatch(closeIssueViewer());
+  }
+
+  const handleClick = (e) => {
+    let selectedFilter = e.currentTarget.getAttribute('id');
+    dispatch(setFilter(selectedFilter));
+    console.log('selected', selectedFilter);
+    filterStyle.forEach((filter) => {
+      if (selectedFilter === filter.type) {
+        // change filter to selected
+        filter.isSelected = true;
+      } else {
+        // change filter to unselected
+        filter.isSelected = false;
       }
-    }
-  }
-  handleCloseViewer = (e) => {
-    this.props.closeIssueViewer();
-  }
+    })
+  };
 
-  handleClick = (e) => {
-    switch (e.target.getAttribute('id')) {
-      case 'all':
-          this.props.setFilter('all');
-          this.setState({
-            filterStyle: {
-              all: 'issue-page-filter-selected',
-              open: 'issue-page-filter-unselected',
-              closed: 'issue-page-filter-unselected',
-              pulls: 'issue-page-filter-unselected',
-            }
-          });
-          break;
-      case 'open':
-          this.props.setFilter('open');
-          this.setState({
-            filterStyle: {
-              all: 'issue-page-filter-unselected',
-              open: 'issue-page-filter-selected',
-              closed: 'issue-page-filter-unselected',
-              pulls: 'issue-page-filter-unselected',
-            }
-          });
-          break;
-      case 'closed':
-          this.props.setFilter('closed');
-          this.setState({
-            filterStyle: {
-              all: 'issue-page-filter-unselected',
-              open: 'issue-page-filter-unselected',
-              closed: 'issue-page-filter-selected',
-              pulls: 'issue-page-filter-unselected',
-            }
-          });
-          break;
-      case 'pulls':
-          this.props.setFilter('pulls');
-          this.setState({
-            filterStyle: {
-              all: 'issue-page-filter-unselected',
-              open: 'issue-page-filter-unselected',
-              closed: 'issue-page-filter-unselected',
-              pulls: 'issue-page-filter-selected',
-            }
-          });
-          break;
-      default:
-          this.props.setFilter('all');
-          this.setState({
-            filterStyle: {
-              all: 'issue-page-filter-unselected',
-              open: 'issue-page-filter-unselected',
-              closed: 'issue-page-filter-unselected',
-              pulls: 'issue-page-filter-unselected',
-            }
-          });
-    }
-    
-  }
+  const filterButtons = filterStyle.map(filter => 
+    <button 
+      onClick={handleClick}
+      key={filter.type} 
+      id={filter.type} 
+      className={filter.isSelected ? selectedStyle : unselectedStyle}>{filter.title}
+    </button>
+  )
 
-
-  render() {
-    return (
-      <div onClick={this.handleClick} className='issue-page-filter-container'>
-        <button id='all' className={this.state.filterStyle.all}>All Issues</button>
-        <button id='open' className={this.state.filterStyle.open}>Open Issues</button>
-        <button id='closed' className={this.state.filterStyle.closed}>Closed Issues</button>
-        <button id='pulls' className={this.state.filterStyle.pulls}>Pull Requests</button>
-        <button id='exit' onClick={this.handleCloseViewer}> 
-          <img src = {closeIcon} className='close-icon' alt='Close issue viewer' />
+  return (
+      <div className='issue-page-filter-container'>
+        {filterButtons}
+        <button key='exit' id='exit-button' onClick={handleCloseViewer}> 
+          <img src={closeIcon} className='close-icon' alt='Close issue viewer' />
         </button>
       </div>
-      )
-  }
+  )
 }
 
-const mapStateToProps = state => ({
-  filterSelected: state.filterSelected,
-});
-export default connect(mapStateToProps, {closeIssueViewer, setFilter})(IssuePageFilter);
+export default IssuePageFilter;
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,23 @@ export const CLOSE_ISSUE_VIEWER = 'CLOSE_ISSUE_VIEWER';
 export const LOAD_ISSUES = 'LOAD_ISSUES';
 export const NEXT_PAGE = 'NEXT_PAGE';
 export const PREVIOUS_PAGE = 'PREVIOUS_PAGE';
+export const FILTERS = [{
+    type: 'all',
+    title: 'All Issues',
+    isSelected: true,
+},
+{
+    type: 'open',
+    title: 'Open Issues',
+    isSelected: false,
+},
+{
+    type: 'closed',
+    title: 'Closed Issues',
+    isSelected: false,
+},
+{
+    type: 'pulls',
+    title: 'Pull Requests',
+    isSelected: false,
+},]
