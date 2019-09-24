6bc0251fbdbd67d20dbcd972b4923cfe13a08716
Sarah Ng <sarah.ng26@gmail.com>
2019-09-24 16:51:59 -0700

    Refactored getIssueBody and getIcon

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -3,41 +3,45 @@ import { useSelector } from 'react-redux';
 import IssueLabels from './IssueLabels';
 import { ReactComponent as IssueClosedIcon } from '../_assets/icons/issue-closed.svg';
 import { ReactComponent as PullRequestIcon } from '../_assets/icons/pull-request.svg';
+import { NO_DESCRIPTION_TEXT } from '../constants/constants';
 
-function Issue(props) {
+function Issue (props) {
   const filterSelected = useSelector(state => state.filterSelected);
   const { state, pullRequest, title, body, labels } = props;
   
-  let issueBody;
-  if (body === '') {
-    issueBody = <div className='card-body-text card-body-text-empty'>No description provided.</div>
-  } else {
-    issueBody = <div className='card-body-text'>{body}</div>
-  }
+  const getIssueBody = () => {
+    if (body === '') {
+      return (<div className='card-body-text card-body-text-empty'>{NO_DESCRIPTION_TEXT}</div>);
+    } else {
+      return (<div className='card-body-text'>{body}</div>);
+    }
+  }  
 
-  let icon = <div></div>;
-  if (typeof pullRequest === 'undefined' && state === 'closed') {
-    icon = <IssueClosedIcon className='icon'/>
-  } 
-  if (filterSelected === 'pulls' || (filterSelected !== 'pulls' && typeof pullRequest !== 'undefined'))  {
-    icon = <PullRequestIcon className='icon'/>
+  const getIcon = () => {
+    if (filterSelected !== 'pulls' && typeof pullRequest === 'undefined' && state === 'closed') {
+      return <IssueClosedIcon className='icon'/>
+    } 
+    if (filterSelected === 'pulls' || (filterSelected !== 'pulls' && typeof pullRequest !== 'undefined'))  {
+      return <PullRequestIcon className='icon'/>
+    }
+    return <div></div>;
   }
     
   return (
-  <div className='card-wrapper'>
-    <div className='card-title'>
-      <div className='card-title-text'>
-      {title}
+    <div className='card-wrapper'>
+      <div className='card-title'>
+        <div className='card-title-text'>
+          {title}
+        </div>
+        <div className='svg-wrapper issue-icon'>
+          {getIcon()}
+        </div>
       </div>
-      <div className='svg-wrapper issue-icon'>
-        {icon}
+      <div className='card-details'>
+        {getIssueBody()} 
+        <IssueLabels labels={labels}/>
       </div>
-      </div>
-    <div className='card-details'>
-      {issueBody} 
-    <IssueLabels labels={labels}/>
     </div>
-  </div>
   )
 }
 
