c28ccd5d1b39f41a53992f943fc9621c0741fdfc
Sarah Ng <sarah.ng26@gmail.com>
2019-09-24 16:49:01 -0700

    Added APP_NAME to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,6 +5,7 @@ export const LOAD_ISSUES = 'LOAD_ISSUES';
 export const NEXT_PAGE = 'NEXT_PAGE';
 export const PREVIOUS_PAGE = 'PREVIOUS_PAGE';
 export const NO_DESCRIPTION_TEXT = 'No description provided.';
+export const APP_NAME = 'GitHub Issues Viewer';
 export const FILTERS = [{
     type: 'all',
     title: 'All Issues',
