deda031944a5cc617c2b6ab727f8565831730629
Sarah Ng <sarah.ng26@gmail.com>
2019-09-24 17:43:46 -0700

    Removed redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, composeWithDevTools( ));
+const store = createStore(rootReducer);
+//const store = createStore(rootReducer, composeWithDevTools( ));
 ReactDOM.render(<Provider store = {store}><App /></Provider>, 
   document.getElementById('root'));
 
